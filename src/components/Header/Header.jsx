f887440eb07d04f3e3c9e49a396e14a30a51437c
Tim <topwebknurl@gmail.com>
2023-09-11 22:18:17 +0300

    add header and clean template

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './Header.scss'
+
+
+
+ function Header() {
+
+  return (
+    <header className='heaader'>
+       test
+    </header>
+  )
+}
+
+export default Header
